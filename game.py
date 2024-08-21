e3355b703992e31c8ad528c88f6a8e8473af4b3f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-20 22:42:07 -0300

    upgrade controls

--- game.py
+++ game.py
@@ -32,19 +32,20 @@ bg = pg.transform.scale(bg,(x,y))
 
 player = pg.image.load('img\player.png').convert_alpha()
 player = pg.transform.scale(player,(100,100))
+player = pg.transform.rotate(player,-90)
 
 enemy1 = pg.image.load('img\enemy1.png').convert_alpha()
-enemy1 = pg.transform.scale(enemy1,(50,50))
+enemy1 = pg.transform.scale(enemy1,(70,70))
 
 enemy2 = pg.image.load('img\enemy2.png').convert_alpha()
-enemy2 = pg.transform.scale(enemy2,(50,50))
+enemy2 = pg.transform.scale(enemy2,(60,60))
 
 
 power = pg.image.load('img\power.png')
 power = pg.transform.scale(power,(50,50))
 
 
-position_power_x = 110
+position_power_x = 100
 position_power_y = 400
 
 position_player_x = 100
@@ -61,9 +62,10 @@ speed_x_power = 10
 
 
 running = True
-
+valueup = False
+valuedown = False
 trigged = False
-score_font = pg.font.SysFont('LeagueSpartan-Medium.ttf',60,)
+score_font = pg.font.SysFont('LeagueSpartan-Medium.ttf',60)
 
 # ---------------------------------------------------------------------------
 #game running
@@ -77,12 +79,12 @@ enemy2_ret = enemy2.get_rect()
 #repawn enemies
 def respawn():
     x = 1350
-    y = rd.randint(1,640)
+    y = rd.randint(50,640)
     return [x,y]
 
 def reload_power():
     trigged = False
-    reload_power_x = position_player_x 
+    reload_power_x = position_player_x + 20
     reload_power_y = position_player_y + 26
     speed_x_power = 0
     return[reload_power_x,reload_power_y,trigged,speed_x_power]
@@ -92,7 +94,7 @@ def reload_power():
 def colisions1():
     global points
     if player_ret.colliderect(enemy1_ret) or enemy1_ret.x == 40:
-        points -= 2.5
+        points -= 2
         return True
     elif power_ret.colliderect(enemy1_ret):
         points += 1
@@ -103,23 +105,33 @@ def colisions1():
 def colisions2():
     global points
     if player_ret.colliderect(enemy2_ret) or enemy2_ret.x == 40:
-        points -= 2.5
+        points -= 2
         return True
     elif power_ret.colliderect(enemy2_ret):
         points += 1
         return True
     else:
         return False
-
+#-------------------------------------------------------------------------------------
 #game running
 while running:
     for event in pg.event.get():
-        
-        if event.type == pg.QUIT:
+        if event.type == pg.QUIT or joystick.get_button(6) :
             running = False
-    screen.blit(bg,(0,0))
+            pg.quit()
+            exit()
+        elif event.type == pg.JOYAXISMOTION:
+            if event.value >= 0.5:
+                valuedown = True
+                valueup = False
+            elif event.value <= -0.5:
+                valueup = True
+                valuedown = False
+            else:
+                valueup = False
+                valuedown = False
 
-    
+#-------------------------------------------------------------------------------------    
     rel_x = x % bg.get_rect().width
     screen.blit(bg,(rel_x - bg.get_rect().width,0))
     if rel_x < 1280:
@@ -128,22 +140,20 @@ while running:
     
     #keyboard_controls
     Key_board = pg.key.get_pressed()
-    if (Key_board[pg.K_UP] and position_player_y > 1) or (joystick.get_button(3) and position_player_y > 1):
+    if (Key_board[pg.K_UP] and position_player_y > 1) or (valueup and position_player_y > 1):
         position_player_y -=1
         if not trigged:
             position_power_y -=1
         
 
-    if (Key_board[pg.K_DOWN] and position_player_y < 600) or (joystick.get_button(0) and position_player_y < 600):
+    if (Key_board[pg.K_DOWN] and position_player_y < 600) or (valuedown and position_player_y < 600):
         position_player_y +=1
         if not trigged:
             position_power_y +=1
     
     
-    # if event.type == pg.JOYBUTTONDOWN:
-    #     print(event)    
-    
-    if Key_board[pg.K_SPACE] or joystick.get_button(4) or joystick.get_button(5):
+
+    if Key_board[pg.K_SPACE] or joystick.get_button(0) or joystick.get_button(5):
         trigged = True
         speed_x_power = 5
         
@@ -193,8 +203,8 @@ while running:
     enemy2_ret.y = position_enemy2_y
     
 
-    #move
-    x-= 2
+    #game speed
+    x-= 1
     position_enemy1_x -=1.3
     position_enemy2_x -=1.25
     position_power_x += speed_x_power
@@ -213,7 +223,14 @@ while running:
     screen.blit(player, (position_player_x,position_player_y))
     screen.blit(enemy1, (position_enemy1_x,position_enemy1_y))
     screen.blit(enemy2, (position_enemy2_x,position_enemy2_y))
+    # screen.blit(power,(position_power_x,position_power_y))
+    
 
+    # for event in pg.event.get():
+    #     if event.type == pg.JOYBUTTONDOWN:
+    #         print(event)
+    
+    
     
     # print(points) para conferir no terminal se esta funcionando
     # pg.display.update()
